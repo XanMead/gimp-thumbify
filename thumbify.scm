ad36168623241584905f1c31de4f4c5c50eec818
Xan Mead <x.m.mead@gmail.com>
2015-12-05 16:52:56 -0500

    initial commit; includes script-fu-register information

---
+++ thumbify.scm
@@ -0,0 +1,8 @@
+  (script-fu-register
+    "script-fu-thumbify"
+    "Thumbify"
+    "Extends the boundaries of the image\
+      such that the image is a square.\
+      New space is filled with black."
+    "Xan Mead"
+  )
