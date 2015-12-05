6cfbd5bf27e9e4e4b1b80e344fb4dbf76fccff6e
Xan Mead <x.m.mead@gmail.com>
2015-12-05 18:26:03 -0500

    added further function registration info; excluding parameters

--- thumbify.scm
+++ thumbify.scm
@@ -1,8 +1,11 @@
   (script-fu-register
-    "script-fu-thumbify"
-    "Thumbify"
+    "script-fu-thumbify"                   ;func name
+    "Thumbify"                             ;menu label
     "Extends the boundaries of the image\
       such that the image is a square.\
-      New space is filled with black."
-    "Xan Mead"
+      New space is filled with black."     ;description
+    "Xan Mead"                             ;author
+    "copyright 2015, Xan Mead"             ;copyright notice
+    "October 27, 1997"                     ;date created
+    ""                                     ;image type that the script works on
   )
